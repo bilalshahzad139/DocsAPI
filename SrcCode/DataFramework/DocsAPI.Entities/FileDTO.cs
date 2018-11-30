111f1ecc72f04d13ccafb5ff8582bcb22bb08430
bilalshahzad139 <bilal.shahzad@yahoo.com>
2018-12-01 00:48:19 +0500

    missings files are added now

---
+++ SrcCode/DataFramework/DocsAPI.Entities/FileDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocsAPI.Entities
+{
+    public class FileDTO
+    {
+        public String UniqueName { get; set; }
+        public String ActualFileName { get; set; }
+        public String ContentType { get; set; }
+        public long ConentLengthInBytes { get; set; }
+        public String Extension { get; set; }
+    }
+}
